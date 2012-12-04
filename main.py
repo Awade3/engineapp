3ddaf71e5178c06c4f8c7aaf16dbc4e802e712c3
Alex Wade <alex.wade@mindbodyonline.com>
2012-12-03 16:24:51 -0800

    Added awesome

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ form = """ <form method="post" action="/testform">
 
 rot13Form = """
 <form method="post" action="/rot13">
-<textarea></textarea>
+<textarea name="text">This is awesome!</textarea>
 <input type = "submit">
 </form>
 """
